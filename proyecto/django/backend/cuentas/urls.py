5f84b12727356a47c04b45f3bacfe99d41b803df
Carlos Grisales <root@ip-172-31-48-105.ec2.internal>
2020-05-30 12:13:28 +0000

    registro,acceso y sacada de usuarios ok

---
+++ proyecto/django/backend/cuentas/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('signup/',views.registrarse,name='registrarse'),#vista que se cargara para iniciar el registro de un nuevo usuario
+    path('login/',views.login_u,name='login'),
+    path('logout/',views.logout_u,name='logout'),
+]
---
+++ proyecto/django/backend/cuentas/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,redirect
+
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm #necesario para la creacion de usuarios y para conectarse
+from django.contrib.auth import login,logout #logear usuarios y deslogear
+# Create your views here.
+def registrarse(request):
+    if request.method == 'POST':#si alguien ha mandado los datos ya rellenados del post ira aqui para crear el nuevo usuario
+        formulario= UserCreationForm(request.POST)#guardaremos el formulario enviado en una variable
+        if formulario.is_valid():
+            usuario=formulario.save()#guardar usuario
+            login(request,usuario)#iniciar sesion del usuario creado
+            return redirect('/videos')
+    else:
+        formulario= UserCreationForm()
+    return render(request,'registrarse.html',{'form': formulario})#redirigir a la pagina de registro de usuarios
+
+def login_u(request):#iniciar sesion de usuario
+    if request.method == 'POST':
+        formularioAu=AuthenticationForm(data=request.POST)
+        if formularioAu.is_valid():
+            usuario=formularioAu.user()
+            login(request,usuario)#iniciar sesion del usuario
+            if 'next' in request.POST:#si ha intentado entrar a otra pagina sin loguearse vendra aqui y volvera a la misma despues de loguearse
+                return redirect(request.POST.get('next'))#esto lo redirige a la pagina que habia intentado entrar sin loguearse
+            else:
+                return redirect('/videos')
+    else:
+        formularioAu=AuthenticationForm()
+    return render(request,'login.html',{'form':formularioAu})
+
+def logout_u(request):
+    if request.method == 'POST':
+        logout(request)
+    return  redirect('/videos')
